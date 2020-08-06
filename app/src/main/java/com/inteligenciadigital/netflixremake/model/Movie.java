92d668d77f8c6ed34583c84a9b046402b7555e33
gabriel <jeronimogbariel@gmail.com>
2020-08-06 13:27:26 -0300

    criando base para lista de movies

--- app/src/main/java/com/inteligenciadigital/netflixremake/model/Movie.java
+++ app/src/main/java/com/inteligenciadigital/netflixremake/model/Movie.java
@@ -1,6 +1,14 @@
 package com.inteligenciadigital.netflixremake.model;
 
 public class Movie {
-	private
+	private String coverUrl;
+
+	public String getCoverUrl() {
+		return coverUrl;
+	}
+
+	public void setCoverUrl(String coverUrl) {
+		this.coverUrl = coverUrl;
+	}
 }
 
